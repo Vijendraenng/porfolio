9492c84c9c6fde6cdd0455097f37894d2c51b5ad
vijendraenng <vijendra.nbjwork@gmail.com>
2026-06-29 18:55:01 +0530

    fix: rename contact.js to Contact.js case sensitive fix

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const transporter = require("../config/mailer");
 
 // POST /api/contact
